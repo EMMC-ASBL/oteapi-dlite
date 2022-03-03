b96f233a5eec0a5a39c5569e951ca54a30df94db
Jesper Friis <jesper.friis@sintef.no>
2022-03-03 14:47:11 +0100

    Added a simple filter that adds a collection to the session

---
+++ oteapi_dlite/strategies/filter.py
@@ -0,0 +1,45 @@
+"""Trivial filter that adds an empty collection to the session."""
+# pylint: disable=no-self-use,unused-argument
+from dataclasses import dataclass
+from typing import TYPE_CHECKING
+
+import dlite
+from oteapi.models import SessionUpdate
+
+if TYPE_CHECKING:
+    from typing import Any, Dict, Optional
+
+    from oteapi.models import FilterConfig
+
+
+@dataclass
+class CreateCollectionStrategy:
+    """Trivial filter that adds an empty collection to the session.
+
+    **Registers strategies**:
+
+    - `("filterType", "create_collection")`
+
+    """
+
+    filter_config: "FilterConfig"
+
+    def initialize(
+        self, session: "Optional[Dict[str, Any]]" = None
+    ) -> "Dict[str, Any]":
+        """Initialize."""
+        if session is None:
+            raise ValueError("Missing session")
+        if "collection_id" in session:
+            raise KeyError("`collection_id` already exists in session.")
+        coll = dlite.Collection()
+
+        # Save reference to  the collection to ensure that it lives as long as
+        # the session does
+        session["_collection_ref"] = coll
+
+        return SessionUpdate(collection_id=coll.uuid)
+
+    def get(self, session: "Optional[Dict[str, Any]]" = None) -> "Dict[str, Any]":
+        """Execute the strategy."""
+        return SessionUpdate(collection_id=dlite.Collection())
---
+++ tests/strategies/test_filter.py
@@ -0,0 +1,21 @@
+"""Tests filter strategies."""
+
+
+def test_create_collection():
+    """Test the create_collection filter."""
+    import dlite
+    from oteapi.models.filterconfig import FilterConfig
+
+    from oteapi_dlite.strategies.filter import CreateCollectionStrategy
+
+    config = FilterConfig(filterType="create_collection")
+
+    session = {}
+
+    collfilter = CreateCollectionStrategy(config)
+    session.update(collfilter.initialize(session))
+
+    assert "collection_id" in session
+    coll_id = session["collection_id"]
+    coll = dlite.get_collection(coll_id)
+    assert isinstance(coll, dlite.Collection)
