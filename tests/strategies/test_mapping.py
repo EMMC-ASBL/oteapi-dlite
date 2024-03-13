4a9d6f41c523f8b5ec0a94669657ae0c71c00b9f
TreesaJ <treesarjc@gmail.com>
2024-03-13 15:39:38 +0100

    fix collection

--- tests/strategies/test_mapping.py
+++ tests/strategies/test_mapping.py
@@ -2,7 +2,7 @@
 def test_mapping_without_prefixes() -> None:
     """Test without prefixes."""
     from tripper import EMMO, MAP, Namespace
-
+    import dlite
     from oteapi_dlite.strategies.mapping import (
         DLiteMappingConfig,
         DLiteMappingStrategy,
@@ -12,19 +12,21 @@ def test_mapping_without_prefixes() -> None:
     FORCES = Namespace("http://onto-ns.com/meta/0.1/Forces#")
     ENERGY = Namespace("http://onto-ns.com/meta/0.1/Energy#")
 
+    coll = dlite.Collection()
     config = DLiteMappingConfig(
         mappingType="mappings",
         triples=[
             (FORCES.forces, MAP.mapsTo, EMMO.Force),
             (ENERGY.energy, MAP.mapsTo, EMMO.PotentialEnergy),
         ],
+        collection_id= coll.uuid,
     )
 
     mapper = DLiteMappingStrategy(config)
-    session = mapper.initialize()
-    session.update(mapper.get())
+    mapper.initialize()
+    mapper.get()
 
-    collection = get_collection(session)
+    collection = get_collection(collection_id= coll.uuid)
     relations = set((str(s), str(p), str(o)) for s, p, o in collection.get_relations())
     assert (str(FORCES.forces), str(MAP.mapsTo), str(EMMO.Force)) in relations
     assert (str(ENERGY.energy), str(MAP.mapsTo), str(EMMO.PotentialEnergy)) in relations
@@ -33,7 +35,7 @@ def test_mapping_without_prefixes() -> None:
 def test_mapping_with_prefixes() -> None:
     """Test with prefixes."""
     from tripper import EMMO, MAP, Namespace
-
+    import dlite
     from oteapi_dlite.strategies.mapping import (
         DLiteMappingConfig,
         DLiteMappingStrategy,
@@ -42,7 +44,7 @@ def test_mapping_with_prefixes() -> None:
 
     FORCES = Namespace("http://onto-ns.com/meta/0.1/Forces#")
     ENERGY = Namespace("http://onto-ns.com/meta/0.1/Energy#")
-
+    coll = dlite.Collection()
     config = DLiteMappingConfig(
         mappingType="mappings",
         prefixes={
@@ -53,13 +55,14 @@ def test_mapping_with_prefixes() -> None:
             (FORCES.forces, MAP.mapsTo, EMMO.Force),
             (ENERGY.energy, MAP.mapsTo, EMMO.PotentialEnergy),
         ],
+        collection_id= coll.uuid,
     )
 
     mapper = DLiteMappingStrategy(config)
-    session = mapper.initialize()
-    session.update(mapper.get())
+    mapper.initialize()
+    mapper.get()
 
-    collection = get_collection(session)
+    collection = get_collection(collection_id= coll.uuid)
     relations = set((str(s), str(p), str(o)) for s, p, o in collection.get_relations())
     assert len(list(collection.get_relations())) == len(relations)
     assert (str(FORCES.forces), str(MAP.mapsTo), str(EMMO.Force)) in relations
