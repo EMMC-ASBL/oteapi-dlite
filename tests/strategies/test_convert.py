d67c3949f3f11032ba94b9ad589b16bb1f278c92
Francesca.L.Bleken@sintef.no <francesca.l.bleken@sintef.no>
2024-07-03 16:36:11 +0200

    test_convert into a separate pytest

--- tests/strategies/test_convert.py
+++ tests/strategies/test_convert.py
@@ -3,63 +3,67 @@
 from otelib import OTEClient
 from paths import inputdir, outputdir  # pylint: disable=import-error
 
-resultfile = outputdir / "result.yaml"
 
-client = OTEClient("python")
+def test_convert():
+    """
+    Test convert strategy
+    """
+    resultfile = outputdir / "result.yaml"
 
-energy_resource = client.create_dataresource(
-    downloadUrl=(inputdir / "energy.yaml").as_uri(),
-    mediaType="application/vnd.dlite-parse",
-    configuration={
-        "driver": "yaml",
-        "options": "mode=r",
-        "label": "energy",
-    },
-)
+    client = OTEClient("python")
 
-forces_resource = client.create_dataresource(
-    downloadUrl=(inputdir / "forces.yaml").as_uri(),
-    mediaType="application/vnd.dlite-parse",
-    configuration={
-        "driver": "yaml",
-        "options": "mode=r",
-        "label": "forces",
-    },
-)
+    energy_resource = client.create_dataresource(
+        downloadUrl=(inputdir / "energy.yaml").as_uri(),
+        mediaType="application/vnd.dlite-parse",
+        configuration={
+            "driver": "yaml",
+            "options": "mode=r",
+            "label": "energy",
+        },
+    )
 
-convert = client.create_function(
-    functionType="application/vnd.dlite-convert",
-    configuration={
-        "module_name": "test_package.convert_module",
-        "function_name": "converter",
-        "inputs": [
-            {"label": "energy"},
-            {"label": "forces"},
-        ],
-        "outputs": [
-            {"label": "result"},
-        ],
-    },
-)
+    forces_resource = client.create_dataresource(
+        downloadUrl=(inputdir / "forces.yaml").as_uri(),
+        mediaType="application/vnd.dlite-parse",
+        configuration={
+            "driver": "yaml",
+            "options": "mode=r",
+            "label": "forces",
+        },
+    )
 
-generate = client.create_function(
-    functionType="application/vnd.dlite-generate",
-    configuration={
-        "driver": "yaml",
-        "location": str(resultfile),
-        "options": "mode=w",
-        "label": "result",
-    },
-)
+    convert = client.create_function(
+        functionType="application/vnd.dlite-convert",
+        configuration={
+            "module_name": "test_package.convert_module",
+            "function_name": "converter",
+            "inputs": [
+                {"label": "energy"},
+                {"label": "forces"},
+            ],
+            "outputs": [
+                {"label": "result"},
+            ],
+        },
+    )
 
+    generate = client.create_function(
+        functionType="application/vnd.dlite-generate",
+        configuration={
+            "driver": "yaml",
+            "location": str(resultfile),
+            "options": "mode=w",
+            "label": "result",
+        },
+    )
 
-# Remove result file, so that we can check that it is generated
-if resultfile.exists():
-    resultfile.unlink()
+    # Remove result file, so that we can check that it is generated
+    if resultfile.exists():
+        resultfile.unlink()
 
-# Run pipeline
-pipeline = energy_resource >> forces_resource >> convert >> generate
-pipeline.get()
+    # Run pipeline
+    pipeline = energy_resource >> forces_resource >> convert >> generate
+    pipeline.get()
 
-# Ensure that the result file is regenerated
-assert resultfile.exists()
+    # Ensure that the result file is regenerated
+    assert resultfile.exists()
