590200feb30f7d19bc0ffdba076a2faf3956b5f0
Treesa Joseph <66374813+Treesarj@users.noreply.github.com>
2024-07-10 14:30:02 +0200

    Update oceanlab_influx_parser.py

--- oteapi_dlite/strategies/oceanlab_influx_parser.py
+++ oteapi_dlite/strategies/oceanlab_influx_parser.py
@@ -11,7 +11,7 @@ import jinja2
 from fastapi import logger
 from oteapi.models import AttrDict, HostlessAnyUrl, ParserConfig, ResourceConfig
 from pandas import DataFrame
-from pydantic import BaseModel, Field, SecretStr
+from pydantic import Field, SecretStr
 from pydantic.dataclasses import dataclass
 
 from oteapi_dlite.models import DLiteSessionUpdate
@@ -34,7 +34,7 @@ class Measurement(BaseModel):
     ]
 
 
-class MeasurementConfig(BaseModel):
+class MeasurementConfig(dict):
     """Measurement config"""
 
     measurement: Annotated[
