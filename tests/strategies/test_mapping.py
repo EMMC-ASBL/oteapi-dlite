8dedb0bcfc2b3ea1586a0fd6dea36dc3f63467a1
Francesca.L.Bleken@sintef.no <francesca.l.bleken@sintef.no>
2025-01-24 16:01:45 +0100

    Tripper>=0.3.5 resolves EMMO namespace correctly.
    This means that using EMMO.prefLabel actually leads to the
    full and correct IRI.

--- tests/strategies/test_mapping.py
+++ tests/strategies/test_mapping.py
@@ -64,11 +64,19 @@ def test_mapping_with_prefixes() -> None:
             "emmo": str(EMMO),
         },
         triples=[
-            ("f:forces", "map:mapsTo", "emmo:Force"),
-            ("e:energy", "map:mapsTo", "emmo:PotentialEnergy"),
+            # From tripper 0.3.5 importing EMMO as namespace resolves correctly
+            (
+                "f:forces",
+                "map:mapsTo",
+                "https://w3id.org/emmo#EMMO_1f087811_06cb_42d5_90fb_25d0e7e068ef",
+            ),
+            (
+                "e:energy",
+                "map:mapsTo",
+                "https://w3id.org/emmo#EMMO_4c151909_6f26_4ef9_b43d_7c9e9514883a",
+            ),
         ],
     )
-
     session = DLiteMappingStrategy(config).initialize()
 
     # Remove the line immediately below once EMMC-ASBL/oteapi#545 is fixed
